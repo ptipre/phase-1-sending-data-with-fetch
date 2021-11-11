6fafea28053ed0a2e86b510f69864f02a6639f75
Prajay Tipre <prajaytipre@gmail.com>
2021-11-11 13:35:03 -0500

    commit tree c2b775e136650a628497509f1eaa6da48842d1d9

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+const { bodyParser } = require("json-server")
+
+function submitData(userName, userEmail) {
+
+    let config = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+
+            name: `${userName}`,
+            email: `${userEmail}`,
+        })
+    }
+    
+    
+    
+    return fetch('http://localhost:3000/users', config)
+    .then(resp => resp.json())
+    .then(userInfo => {
+
+        let id = userInfo.id;
+        let newID = document.createElement('p');
+        let body = document.querySelector('body');
+        
+        newID.textContent = id;
+
+        body.appendChild(newID)
+
+
+    })
+    .catch(error => {
+
+        let body = document.querySelector('body');
+        let errorMessage = document.createElement('p');
+
+        errorMessage.textContent = error.message
+
+        body.appendChild(errorMessage)
+    })
+}
